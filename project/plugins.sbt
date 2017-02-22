86edd4d310508cdfecdc504d211ff9566ef8cd8a
ladrl <Rugwarrior34?>
2017-02-22 21:38:36 +0100

    Connect in Game

--- project/plugins.sbt
+++ project/plugins.sbt
@@ -1,3 +1,8 @@
 logLevel := Level.Warn
 
+
+resolvers ++= Seq(
+  "Artima Maven Repository" at "http://repo.artima.com/releases"
+)
+
 addSbtPlugin("com.artima.supersafe" % "sbtplugin" % "1.1.0")
--- src/main/scala/Harvester.scala
+++ src/main/scala/Harvester.scala
@@ -1,71 +1,62 @@
 import java.net.InetSocketAddress
 
-import akka.actor.Actor.Receive
 import akka.actor.{Actor, ActorLogging, ActorRef, ActorSystem, Props}
+import akka.event.LoggingReceive
+import akka.util.ByteString
+import argonaut.Argonaut._
 import com.typesafe.config.ConfigFactory
 import net.sigusr.mqtt.api._
 
+class Game(teamName: String) extends Actor with ActorLogging {
 
-class Game(teamName: String, mqttMgr: ActorRef) extends  Actor with ActorLogging {
+  context.actorOf(Manager.props(new InetSocketAddress("127.0.0.1", 1883)), "mqtt-broker") ! Connect(self.path.name)
+
   val baseTopic = s"player/$teamName"
   val incomingTopic = s"$baseTopic/incoming"
   val gameTopic = s"$baseTopic/game"
 
-  log.info("Subscribing")
-  mqttMgr ! Subscribe(Vector((baseTopic, AtMostOnce), (incomingTopic, AtMostOnce)), 0)
-
-  val robot = context.actorOf(Props(new Robot(mqttMgr)))
-
-
   override def receive: Receive = {
-    case Subscribed(topics, _) =>
-      log.info(s"Subscribed to $topics")
-      context become registered()
+    case Connected => context become registering(sender())
   }
 
 
-  def registered():Receive = {
+  def registering(mqttMgr: ActorRef): Receive = {
+    log.info("Subscribing")
+    mqttMgr ! Subscribe(Vector((baseTopic, AtMostOnce), (incomingTopic, AtMostOnce)), 1)
+    mqttMgr ! Publish(baseTopic, ByteString(RoboChallengeMessage.register.asJson.nospaces).toVector)
 
-    {
-      case "start" =>
+    LoggingReceive {
+      case Subscribed(topics, MessageId(1)) =>
+        log.info(s"Subscribed to $topics")
+      case Message(t, content) if t == baseTopic =>
+        val cmd = ByteString.fromArray(content.toArray).toString.decodeOption[RoboChallengeMessage.Command]
+
+        log.info(s"Got $cmd")
+        context become registered()
+    }
+  }
+
+  def registered(): Receive = {
+    val robot = context.actorOf(Props(new Robot(ActorRef.noSender)))
+
+    LoggingReceive {
+      case Message(t, content) if t == baseTopic =>
+      case Message(t, content) if t == incomingTopic =>
+      case Message(t, content) if t == gameTopic =>
 
     }
   }
 }
 
-
-class Harvester(teamName: String) extends Actor with ActorLogging {
-
-  val mqttBroker = context.actorOf(Manager.props(new InetSocketAddress("127.0.0.1", 1883)), "mqtt-broker")
-
-  mqttBroker ! net.sigusr.mqtt.api.Connect("Harvester")
-
-  def receive: Receive = {
-    case Connected ⇒
-      log.info("Successfully connected")
-      context become connected(sender())
-    case ConnectionFailure(reason) ⇒
-      println(s"Connection to localhost:1883 failed [$reason]")
-  }
-
-
-  def connected(mqttMgr: ActorRef): Receive = {
-    val game = context.actorOf(Props(new Game(teamName, mqttMgr)))
-
-    {
-      case Disconnected =>
-    }
-  }
-}
 
 object Harvester {
 
   val config =
     """akka {
-         loglevel = INFO
+         loglevel = DEBUG
          actor {
             debug {
-              receive = on
+              receive = off
               autoreceive = off
               lifecycle = off
             }
@@ -75,14 +66,14 @@ object Harvester {
 
 
   val teamName = "akkaers"
-  val system = ActorSystem(s"$teamName-system" , ConfigFactory.parseString(config))
+  val system = ActorSystem(s"$teamName-system", ConfigFactory.parseString(config))
 
   def shutdown(): Unit = {
     system.shutdown()
   }
 
-  def main(args: Array[String]):Unit = {
-    system.actorOf(Props(classOf[Harvester], teamName), "harvester")
+  def main(args: Array[String]): Unit = {
+    system.actorOf(Props(new Game(teamName)))
     sys.addShutdownHook {
       shutdown()
     }
