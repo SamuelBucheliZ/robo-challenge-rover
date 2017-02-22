e50be254c0e4ecf62ff573317ff6ba53d30c7c98
SamuelBucheliZ <Samuel.Bucheli@zuehlke.com>
2017-02-22 21:04:55 +0100

    First tests

--- src/test/scala/RoboChallengeMessageSpec.scala
+++ src/test/scala/RoboChallengeMessageSpec.scala
@@ -1,9 +1,22 @@
+import RoboChallengeMessage.Command
 import org.scalatest.FlatSpec
+import argonaut._, Argonaut._
 
 class RoboChallengeMessageSpec extends FlatSpec {
 
-  "An empty Set" should "have size 0" in {
-    assert(Set.empty.size == 0)
+   val registerComandJson = "{\"command\":\"register\",\"args\":[]}"
+
+  "Register command" should "be parsed correctly" in {
+    val parsedCommand = registerComandJson.decodeOption[Command]
+    assert(parsedCommand.isDefined)
+    val registerCommand = parsedCommand.get
+    assert(registerCommand.command == "register")
+  }
+
+  "Register command" should "be written as JSON correctly" in {
+    val registerCommand = Command("register", List())
+    val json = registerCommand.asJson
+    assert(json.nospaces == registerComandJson)
   }
 
 }
